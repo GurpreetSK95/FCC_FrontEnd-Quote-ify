a1cf8ef30cfe7d69b236990d684cc9c370da2778
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-01-14 01:00:04 +0530

    feat: basic setup, issue in API

--- js/baseScript.js
+++ js/baseScript.js
@@ -1,3 +1,15 @@
 /**
  * Created by gurpreet on 14/01/17.
  */
+
+$(document).ready(function () {
+   $("#quote_button").on("click", function () {
+       //TODO: issue in api
+       $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+       function (json) {
+           // var content = val[0].content;
+           // var title = val[0].title;
+           $(".quote").html(JSON.stringify(json));
+       });
+   });
+});
